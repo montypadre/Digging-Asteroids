542ee3f163d6168a694a55456dce624a08a80cd1
Mike <montypadre77@gmail.com>
2022-09-07 21:46:17 -0500

    Fixed chomp mechanic, trying two separate mouse triggers

--- Assets/Scripts/Player/JawsBehavior.cs
+++ Assets/Scripts/Player/JawsBehavior.cs
@@ -9,146 +9,142 @@ public class JawsBehavior : MonoBehaviour
 	public bool isGrabbing = false;
 	public ParticleSystem chompParticle;
 	public MedRock medRock;
-	private Collider enemy;
+	private Collider col;
+	private GameObject currentObj;
 
 	private void Update()
 	{
-		if (isEnemy && enemy != null)
+		Debug.Log(currentObj);
+		if (col != null)
 		{
-			if (enemy.gameObject.CompareTag("Rock"))
+			if (Input.GetMouseButtonDown(0) && col.CompareTag("Rock"))
 			{
-				if (Input.GetMouseButtonDown(0))
-				{
-					enemy.gameObject.GetComponent<MedRock>().Chomp();
-					Instantiate(chompParticle, transform.position, transform.rotation);
-				}
-				//else if (Input.GetMouseButtonUp(0))
-				//{
-				//	//if (enemy.gameObject.CompareTag("Gem") || enemy.gameObject.CompareTag("Bomb"))
-				//	//{
-				//	//	isGrabbing = false;
-				//	//	gameObject.GetComponent<GrabObjects>().ReleaseObject(enemy.gameObject);
-				//	//}
-				//	//Debug.Log(enemy);
-				//	switch (enemy.gameObject.name)
-				//	{
-				//		case "Bomb(Clone)":
-				//			gameObject.GetComponent<GrabObjects>().ReleaseObject(enemy.gameObject);
-				//			break;
-				//		case "purple(Clone)":
-				//			gameObject.GetComponent<GrabObjects>().ReleaseObject(enemy.gameObject);
-				//			break;
-				//		case "sapphire(Clone)":
-				//			gameObject.GetComponent<GrabObjects>().ReleaseObject(enemy.gameObject);
-				//			break;
-				//		case "emerald(Clone)":
-				//			gameObject.GetComponent<GrabObjects>().ReleaseObject(enemy.gameObject);
-				//			break;
-				//		case "star(Clone)":
-				//			gameObject.GetComponent<GrabObjects>().ReleaseObject(enemy.gameObject);
-				//			break;
-				//		default:
-				//			break;
-				//	}
-				//}
-				//	{
-				//if (enemy.gameObject.CompareTag("Rock"))
-				//{
-				//	enemy.gameObject.GetComponent<MedRock>().Chomp();
-				//	Instantiate(chompParticle, transform.position, transform.rotation);
-				//}
-				//else if (enemy.gameObject.CompareTag("Gem") || enemy.gameObject.CompareTag("Bomb"))
-				//{
-				//	Debug.Log(isGrabbing);
-				//	isGrabbing = true;
-				//	gameObject.GetComponent<GrabObjects>().GrabObject(enemy.gameObject);
-				//}
-				//	Debug.Log(enemy);
-				//switch (enemy.gameObject.name)
-				//{
-				//	case "Med Rock(Clone)":
-				//		enemy.gameObject.GetComponent<MedRock>().Chomp();
-				//		Instantiate(chompParticle, transform.position, transform.rotation);
-				//		break;
-				//	case "Bomb(Clone)":
-				//		gameObject.GetComponent<GrabObjects>().GrabObject(enemy.gameObject);
-				//		break;
-				//	case "purple(Clone)":
-				//		gameObject.GetComponent<GrabObjects>().GrabObject(enemy.gameObject);
-				//		break;
-				//	case "sapphire(Clone)":
-				//		gameObject.GetComponent<GrabObjects>().GrabObject(enemy.gameObject);
-				//		break;
-				//	case "emerald(Clone)":
-				//		gameObject.GetComponent<GrabObjects>().GrabObject(enemy.gameObject);
-				//		break;
-				//	case "star(Clone)":
-				//		gameObject.GetComponent<GrabObjects>().GrabObject(enemy.gameObject);
-				//		break;
-				//	default:
-				//		break;
-				//}
+				col.gameObject.GetComponent<MedRock>().Chomp();
+				Instantiate(chompParticle, transform.position, transform.rotation);
+			}
+			if (Input.GetMouseButtonDown(1) && !col.CompareTag("Rock"))
+			{
+				currentObj = col.gameObject;
+				gameObject.GetComponent<GrabObjects>().GrabObject(col.gameObject);
+			}
+			if (Input.GetMouseButtonUp(1) )
+			{
+				gameObject.GetComponent<GrabObjects>().ReleaseObject(currentObj);
 			}
 		}
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
-		enemy = other;
-		isEnemy = true;
+		col = other;
 	}
 
-	void OnTriggerExit(Collider other)
-	{
-		isEnemy = false;
-	}
+	//private void Update()
+	//{
+	//	if (isEnemy && enemy != null)
+	//	{
+	//		if (enemy.gameObject.CompareTag("Rock"))
+	//		{
+	//			if (Input.GetMouseButtonDown(0))
+	//			{
+	//				enemy.gameObject.GetComponent<MedRock>().Chomp();
+	//				Instantiate(chompParticle, transform.position, transform.rotation);
+	//			}
+	//			//else if (Input.GetMouseButtonUp(0))
+	//			//{
+	//			//	//if (enemy.gameObject.CompareTag("Gem") || enemy.gameObject.CompareTag("Bomb"))
+	//			//	//{
+	//			//	//	isGrabbing = false;
+	//			//	//	gameObject.GetComponent<GrabObjects>().ReleaseObject(enemy.gameObject);
+	//			//	//}
+	//			//	//Debug.Log(enemy);
+	//			//	switch (enemy.gameObject.name)
+	//			//	{
+	//			//		case "Bomb(Clone)":
+	//			//			gameObject.GetComponent<GrabObjects>().ReleaseObject(enemy.gameObject);
+	//			//			break;
+	//			//		case "purple(Clone)":
+	//			//			gameObject.GetComponent<GrabObjects>().ReleaseObject(enemy.gameObject);
+	//			//			break;
+	//			//		case "sapphire(Clone)":
+	//			//			gameObject.GetComponent<GrabObjects>().ReleaseObject(enemy.gameObject);
+	//			//			break;
+	//			//		case "emerald(Clone)":
+	//			//			gameObject.GetComponent<GrabObjects>().ReleaseObject(enemy.gameObject);
+	//			//			break;
+	//			//		case "star(Clone)":
+	//			//			gameObject.GetComponent<GrabObjects>().ReleaseObject(enemy.gameObject);
+	//			//			break;
+	//			//		default:
+	//			//			break;
+	//			//	}
+	//			//}
+	//			//	{
+	//			//if (enemy.gameObject.CompareTag("Rock"))
+	//			//{
+	//			//	enemy.gameObject.GetComponent<MedRock>().Chomp();
+	//			//	Instantiate(chompParticle, transform.position, transform.rotation);
+	//			//}
+	//			//else if (enemy.gameObject.CompareTag("Gem") || enemy.gameObject.CompareTag("Bomb"))
+	//			//{
+	//			//	Debug.Log(isGrabbing);
+	//			//	isGrabbing = true;
+	//			//	gameObject.GetComponent<GrabObjects>().GrabObject(enemy.gameObject);
+	//			//}
+	//			//	Debug.Log(enemy);
+	//			//switch (enemy.gameObject.name)
+	//			//{
+	//			//	case "Med Rock(Clone)":
+	//			//		enemy.gameObject.GetComponent<MedRock>().Chomp();
+	//			//		Instantiate(chompParticle, transform.position, transform.rotation);
+	//			//		break;
+	//			//	case "Bomb(Clone)":
+	//			//		gameObject.GetComponent<GrabObjects>().GrabObject(enemy.gameObject);
+	//			//		break;
+	//			//	case "purple(Clone)":
+	//			//		gameObject.GetComponent<GrabObjects>().GrabObject(enemy.gameObject);
+	//			//		break;
+	//			//	case "sapphire(Clone)":
+	//			//		gameObject.GetComponent<GrabObjects>().GrabObject(enemy.gameObject);
+	//			//		break;
+	//			//	case "emerald(Clone)":
+	//			//		gameObject.GetComponent<GrabObjects>().GrabObject(enemy.gameObject);
+	//			//		break;
+	//			//	case "star(Clone)":
+	//			//		gameObject.GetComponent<GrabObjects>().GrabObject(enemy.gameObject);
+	//			//		break;
+	//			//	default:
+	//			//		break;
+	//			//}
+	//		}
+	//	}
+	//}
 
-	void OnTriggerStay(Collider other)
+	//void OnTriggerEnter(Collider col)
+	//{
+	//	enemy = col;
+	//	isEnemy = true;
+	//}
+
+	//void OnTriggerExit(Collider col)
+	//{
+	//	isEnemy = false;
+	//}
+
+	void OnTriggerStay(Collider col)
 	{
-		if (Input.GetMouseButtonDown(0))
+		if (Input.GetMouseButtonDown(0) && col.CompareTag("Rock"))
 		{
-			if (other.gameObject.CompareTag("Gem"))
-			{
-				gameObject.GetComponent<GrabObjects>().GrabObject(other.gameObject);
-			}
-			else if (other.gameObject.CompareTag("Bomb"))
-			{
-				isGrabbing = true;
-				gameObject.GetComponent<GrabObjects>().GrabObject(other.gameObject);
-			}
+			col.gameObject.GetComponent<MedRock>().Chomp();
+			Instantiate(chompParticle, transform.position, transform.rotation);
 		}
-		else if (Input.GetMouseButtonUp(0))
+		else if (Input.GetMouseButtonDown(1) && !col.CompareTag("Rock"))
 		{
-			if (other.gameObject.CompareTag("Gem"))
-			{
-				gameObject.GetComponent<GrabObjects>().ReleaseObject(other.gameObject);
-			}
-			else if (other.gameObject.CompareTag("Bomb"))
-			{
-				isGrabbing = false;
-				gameObject.GetComponent<GrabObjects>().ReleaseObject(other.gameObject);
-			}
-				//switch (other.gameObject.name)
-				//{
-				//	case "Bomb(Clone)":
-				//		isGrabbing = false;
-				//		gameObject.GetComponent<GrabObjects>().ReleaseObject(other.gameObject);
-				//		break;
-				//	case "purple(Clone)":
-				//		gameObject.GetComponent<GrabObjects>().ReleaseObject(other.gameObject);
-				//		break;
-				//	case "sapphire(Clone)":
-				//		gameObject.GetComponent<GrabObjects>().ReleaseObject(other.gameObject);
-				//		break;
-				//	case "emerald(Clone)":
-				//		gameObject.GetComponent<GrabObjects>().ReleaseObject(other.gameObject);
-				//		break;
-				//	case "star(Clone)":
-				//		gameObject.GetComponent<GrabObjects>().ReleaseObject(other.gameObject);
-				//		break;
-				//	default:
-				//		break;
-				//}
+			gameObject.GetComponent<GrabObjects>().GrabObject(col.gameObject);
+		}
+		else if (Input.GetMouseButtonUp(1) && !col.CompareTag("Rock"))
+		{
+			gameObject.GetComponent<GrabObjects>().ReleaseObject(col.gameObject);
 		}
 	}
 }
